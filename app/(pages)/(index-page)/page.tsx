6f3b5f51ab5de1368217ab6f9f95afcc063c68b9
brandonw504 <brandonw504@outlook.com>
2024-11-23 23:51:27 -0800

    Added Apollo Client

--- app/(pages)/(index-page)/page.tsx
+++ app/(pages)/(index-page)/page.tsx
@@ -1,9 +1,11 @@
 import Hero from './_components/Hero/Hero';
+import Page from './_components/test/test';
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <Page />
     </main>
   );
 }
